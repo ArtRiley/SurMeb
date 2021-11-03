25e7c160cd67da8b98a63bfa489f6ecab643e143
Riley <art.vidimo.kun@gmail.com>
2021-11-03 19:21:59 +0300

    обновлена шапка

--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -1,3 +1,5 @@
-$('.js-header-dropdown').click((e) => {
-  $(e.currentTarget).closest('.header-menu__item').toggleClass('is-open');
-});
+if ($(window).width() < 1200) {
+  $('.js-header-dropdown').click((e) => {
+    $(e.currentTarget).closest('.header-menu__item').toggleClass('is-open');
+  });
+}
