e74a4d686011664b8520bb7d323c1e963d361022
Riley <art.vidimo.kun@gmail.com>
2021-11-04 00:00:46 +0300

    добавлена возможность открывать подборки

--- src/common.blocks/section-compilation/section-compilation.js
+++ src/common.blocks/section-compilation/section-compilation.js
@@ -9,3 +9,27 @@ catalogTabs.click((e) => {
   catalogCompilations.removeClass('is-active');
   catalogCompilations.eq(index).addClass('is-active');
 });
+
+$('.compilation-list').each((i, list) => {
+  const compilationCards = $(list).find('.compilation-card');
+
+  compilationCards.each((j, card) => {
+    const index = j;
+    const grids = $(card).closest('.section-compilation__array-item').find('.section-compilation__grid');
+
+    $(card).click((e) => {
+      const target = $(e.currentTarget);
+      console.log(target);
+      grids.removeClass('is-active');
+      $(grids[index]).addClass('is-active');
+    });
+  });
+  // compilationCards.click((e) => {
+  //   const target = $(e.currentTarget);
+  //   const index = target.index();
+  //   const grids = target.closest('.section-compilation__array-item')
+  // .find('.section-compilation__grid');
+  //   console.log(index);
+  //   console.log(grids);
+  // });
+});
