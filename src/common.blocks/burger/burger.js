4f74e3c1afaba0e7b6e5e6198998e10c7aa20132
Riley <art.vidimo.kun@gmail.com>
2021-11-05 17:12:45 +0300

    исправлена шапка и мобильное меню

--- src/common.blocks/burger/burger.js
+++ src/common.blocks/burger/burger.js
@@ -1,4 +1,8 @@
 $('.js-burger').click((e) => {
+  const scrollSize = $(window).outerWidth() - $('main').width();
+  $('body').css('padding-right', scrollSize);
+  $('header').css('padding-right', scrollSize);
+  $('html').toggleClass('scroll-hide');
   $(e.currentTarget).toggleClass('is-active');
   $('#menu').toggleClass('is-open');
 });
