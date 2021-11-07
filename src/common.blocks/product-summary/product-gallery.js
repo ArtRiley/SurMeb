b4091bd0fe3fe5730b4188852a5e334470cdbe29
Riley <art.vidimo.kun@gmail.com>
2021-11-07 18:59:39 +0300

    обновлено модальное окно с продуктом

---
+++ src/common.blocks/product-summary/product-gallery.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+var galleryThumbs = new Swiper('.thumbs .swiper-container', {
+  threshold: 5,
+  spaceBetween: 8,
+  slidesPerView: 3,
+  // centeredSlides: false,
+  slideToClickedSlide: true,
+  // centerInsufficientSlides: true,
+  loop: false,
+  freeMode: false,
+  watchSlidesVisibility: true,
+  breakpoints: {
+    768: {
+      spaceBetween: 24,
+    }
+  }
+});
+
+new Swiper ('.swiper-gallery .swiper-container', {
+  direction: 'horizontal',
+  spaceBetween: 0,
+  simulateTouch: true,
+  loop: false,
+  observer: true,
+  observeParents: true,
+  threshold: 5,
+  thumbs: {
+    swiper: galleryThumbs
+  },
+  // pagination: {
+  //   el: '.swiper-gallery .swiper-pagination',
+  //   clickable: true
+  // },
+});
