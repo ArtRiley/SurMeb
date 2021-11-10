9d841db5f99aa7c6b9604f8077474375fb2fa052
Riley <art.vidimo.kun@gmail.com>
2021-11-11 00:17:35 +0300

    баннер на главной переделан в свайпер

---
+++ src/common.blocks/main-banner/main-banner.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+new Swiper ('.main-banner .swiper-container', {
+  direction: 'horizontal',
+  spaceBetween: 0,
+  simulateTouch: true,
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+      delay: 6000,
+  },
+  observer: true,
+  observeParents: true,
+  threshold: 5,
+});
